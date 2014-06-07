3b886db5bcd52141847704179bb210e819f99d79
William Lee <wlee@mochify.com>
2014-06-07 14:35:24 -0400

    Adding the database dependencies, clojure liberator, and the 'teamly' API skeleton. Also a rudimentary design document

---
+++ DESIGN.md
@@ -0,0 +1,5 @@
+* Each team gets a unique identifier and a person can be part of more than one team
+* You can POST a status via a JSON message (format yet to be defined).
+    * You can specify that the message is broadcast to all channels or just to a specific team
+* The status will be the most recent reflected for you for the team stream.
+* The team stream displays the latest statuses from everyone, and can list problems and work.
--- src/coordinate/routes.clj
+++ src/coordinate/routes.clj
@@ -1,15 +1,25 @@
 (ns coordinate.routes
-  (:use compojure.core
-          coordinate.views
-          ring.adapter.jetty
-          ring.middleware.reload
-          ring.middleware.stacktrace)
-  (:require [compojure.handler :as handler]
+  (:use ring.middleware.stacktrace)
+  (:require [liberator.core :refer [resource defresource]] 
+            [compojure.handler :as handler]
             [compojure.route :as route]
-            [compojure.response :as response]))
+            [compojure.response :as response]
+            [compojure.core :refer [defroutes GET POST ANY context]]
+            [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.params :refer [wrap-params]]))
 
-(defroutes app-routes
-  (GET "/" [] "hi"))
+(defroutes coordinate-app
+  (ANY "/" [] (resource))
+  (ANY "/foo" [] (resource :available-media-types ["text/html"]
+                           :handle-ok "<html>Hello, everything's good.</html>"))
+  (context "/api" []
+    (POST "/update/:team" [team] (str team))
+    (GET "/update/:team" [team] (str "<h1>" team "</h1>"))))
 
-(def app
-  (handler/site app-routes))
+;(def app
+;  (handler/site app-routes))
+
+(def handler
+  (-> coordinate-app (wrap-params)))
+
+(run-jetty #'handler {:port 8080})
