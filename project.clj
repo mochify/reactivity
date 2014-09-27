fe70342ae2f56cfe746fd51b316046458fcaef96
William Lee <wlee@mochify.com>
2014-09-27 16:14:52 -0400

    Updating compojure route

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                    [http-kit            "2.1.16"]
                    [http-kit.fake       "0.2.1"]
                    [ring                "1.3.1"]
+                   [ring/ring-json      "0.3.1"]
                    [compojure           "1.1.9"]
                    [hiccup              "1.0.5"]
                    [expectations        "2.0.9"]
@@ -19,7 +20,7 @@
                    [liberator           "0.12.1"]]
     :plugins [[lein-ring "0.8.11"]
               [lein-environ "1.0.0"]]
-    :ring {:handler reactivity.routes/handler}
+    :ring {:handler reactivity.routes/reactivity-routes}
     :profiles
         {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
                              [ring-mock "0.1.5"]
